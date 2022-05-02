69f492ec90550438fd8d434d28b3dea916b04198
Gustavo <gustavohenri316@gmail.com>
2022-05-02 17:13:34 -0300

    add ghost button

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -1,17 +1,28 @@
 import { ButtonHTMLAttributes, ReactNode } from "react"
+import tw from "tailwind-styled-components"
 
-interface Props extends ButtonHTMLAttributes<HTMLButtonElement>  {
-  children: ReactNode
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: ReactNode;
+  variant?: 'ghost'
 }
 
-export const Button = ({ children, ...props}: Props) => {
+export const Button = ({ children, variant, ...props }: Props) => {
+ 
   return (
     <>
-      <button
-        {...props}
-        className="bg-gray-900 h-10 text-white font-bold hover:bg-[#00FF4E] disabled:bg-gray-200 disabled:cursor-not-allowed">
+      <BaseButton{...props} variant={variant}>
         {children}
-      </button>
+      </BaseButton>
     </>
   )
 }
+interface StyleProps{
+  variant?: 'ghost'
+}
+export const BaseButton = tw.button<StyleProps>`
+  h-10 px-8 text-white font-bold  disabled:bg-gray-200 
+  disabled:cursor-not-allowed"
+  ${({ variant }) => variant === 'ghost' ? Ghost : Main}
+`
+const Main = ' bg-gray-900 hover:bg-[#00FF4E] hover:text-gray-900'
+const Ghost = 'bg-transparent hover:bg-gray-50 text-gray-900 hover:bg-[#00FF4E]'
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import { Button } from '../../components/atoms/Button'
+import { TemplateContainer } from '../../components/templates'
 
 export const Dashboard = () => {
   return (
-    <div>ola</div>
+    <TemplateContainer>
+      <header className="w-full max-w-screen-lg flex justify-between items-center
+       h-1/6">
+        <span className="font-bold text-2x1">Expanse Tracker</span>
+        <nav className='flex gap-4'>
+          <Button>Adicionar Nova Conta</Button>
+          <Button variant='ghost'>Sair</Button>
+        </nav>
+      </header>
+    </TemplateContainer>
   )
 }
